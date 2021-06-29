d98fb5717bdf2b564360af6fdac0753e61e078a7
BrianH <brianhwang29@gmail.com>
2021-06-29 15:40:02 +0900

    update & delete

--- src/main/java/com/bulletin2/practice/controller/BoardController.java
+++ src/main/java/com/bulletin2/practice/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.bulletin2.practice.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -40,11 +37,30 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @GetMapping("/bbsList/{id}")
+    @GetMapping("/post/{id}")
     public String detail(@PathVariable("id") Long id, Model model){
         BoardDto boardDto = boardService.getPost(id);
 
         model.addAttribute("boardDto", boardDto);
         return "bbsDetail";
+    }
+
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model){
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("boardDto",boardDto);
+        return "bbsEdit";
+    }
+
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String delete(@PathVariable("id") Long id){
+        boardService.deletePost(id);
+        return "redirect:/";
     }
 }
